0cb42c0cf7e860e70ce2b3cc4eb2c253a4364aaf
yixiancoding <yixian.gu@gmail.com>
2026-02-20 19:10:44 -0500

    chore: set up @isaco/tokens package structure

---
+++ packages/tokens/src/index.ts
@@ -0,0 +1,9 @@
+// Export primitives
+export * from './primitives/colors'
+export * from './primitives/typography'
+export * from './primitives/spacing'
+
+// Export semantic tokens
+export * from './semantic/colors'
+export * from './semantic/typography'
+export * from './semantic/spacing'
